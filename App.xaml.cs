0cabc7fe44272217e7f05eb21007fc87fc19e09f
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-08 21:31:33 +0300

    переопределение логики запуска приложения

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,13 +12,22 @@ public partial class App : Application
 {
 
 	private static IServiceProvider? _services;
-	public static IServiceProvider? Services => _services??= InitializingServices().BuildServiceProvider();
+	public static IServiceProvider? Services => _services ??= InitializingServices().BuildServiceProvider();
 
 	private static IServiceCollection InitializingServices()
 	{
 		var services = new ServiceCollection();
 		services.AddSingleton<MainWindowViewModel>();
 		services.AddTransient<SecondaryWindowViewModel>();
+
+		services.AddTransient<MainWindow>(s => new MainWindow { DataContext = s.GetRequiredService<MainWindowViewModel>() });
+		services.AddTransient<SecondaryWindow>(s => new SecondaryWindow { DataContext = s.GetRequiredService<SecondaryWindowViewModel>() });
 		return services;
+	}
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+		Services.GetRequiredService<MainWindow>().Show();
 	}
 }
