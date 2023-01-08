e284e2629ae96c0894e06e018cae617498f77f67
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-08 17:40:34 +0300

    Базовая команда

---
+++ Commands/Base/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TwoWindowsMVVM.Commands.Base
+{
+	internal abstract class Command : ICommand
+	{
+		protected virtual bool CanExecute(object? p) => true;
+		protected abstract void Execute (object? p);
+
+		bool ICommand.CanExecute(object? parameter) => 
+			CanExecute(parameter);
+
+		void ICommand.Execute(object? parameter)
+		{
+			if (((ICommand)this).CanExecute(parameter))
+				Execute (parameter);
+		}
+
+		event EventHandler? ICommand.CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+		}
+	}
+}
