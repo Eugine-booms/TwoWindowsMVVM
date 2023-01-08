688d00a2a16ec9fe5e56c857b155c2abbfea1385
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-08 21:20:48 +0300

    Модель сообщения

---
+++ TextMessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoWindowsMVVM;
+
+internal class TextMessageModel
+{
+	public DateTime	 Time { get;  }
+	public string  Text { get; }
+	
+
+	public TextMessageModel(string message) : this(DateTime.Now, message) { }
+
+	public TextMessageModel(DateTime time, string message) => (this.Time, Text) = (time, message);
+	
+}
