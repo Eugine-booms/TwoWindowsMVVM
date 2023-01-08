d5cf6ea6cdbd5ca5940a04c47510687bd90233b0
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-08 21:19:07 +0300

    Заготока для команд

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,56 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TwoWindowsMVVM.Commands;
 
 namespace TwoWindowsMVVM.ViewModels;
 
 internal class MainWindowViewModel: ViewModelBase
 {
 	public string Title { get; set; } = "Главное окно";
+
+	private string? _message;
+
+	public string? Message
+	{
+		get { return _message; }
+		set { Set(ref _message, value); }
+	}
+	private readonly ObservableCollection<TextMessageModel> _messages = new();
+	public IEnumerable<TextMessageModel> Messages => _messages;
+
+	#region commamds
+	#region SendMessageCommand
+	private LambdaCommand? _sendMessageCommand;
+	public ICommand SendMessageCommand => _sendMessageCommand ??= new(OnSendMessageCommandExecute, prop => prop is string { Length: > 0 });
+
+	private void OnSendMessageCommandExecute(object? p)
+	{
+
+	}
+	#endregion
+
+	#region OpenSecondWindowCommand
+	private LambdaCommand? _openSecondWindowCommand;
+	public ICommand OpenSecondWindowCommand => _openSecondWindowCommand ??= new(OnOpenSecondWindowCommandExecuted, ()=>true);
+
+	private void OnOpenSecondWindowCommandExecuted()
+	{
+
+	}
+	#endregion
+
+	#region ChangeToSecondWindowCommand
+	private LambdaCommand? _changeToSecondWindowCommand;
+	public ICommand ChangeToSecondWindowCommand => _openSecondWindowCommand ??= new(OnChangeToSecondWindowCommandExecuted, () => true);
+
+	private void OnChangeToSecondWindowCommandExecuted()
+	{
+
+	}
+	#endregion
+	#endregion
+
+
+
 }
--- ViewModels/SecondaryWindowViewModel.cs
+++ ViewModels/SecondaryWindowViewModel.cs
@@ -1,6 +1,56 @@
-﻿namespace TwoWindowsMVVM.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TwoWindowsMVVM.Commands;
+
+namespace TwoWindowsMVVM.ViewModels;
 
 internal class SecondaryWindowViewModel : ViewModelBase
 {
-	public string Title { get; set; } = "Второе окно";
+	public string Title { get; set; } = "Главное окно";
+
+	private string? _message;
+
+	public string? Message
+	{
+		get { return _message; }
+		set { Set(ref _message, value); }
+	}
+	private readonly ObservableCollection<TextMessageModel> _messages = new();
+	public IEnumerable<TextMessageModel> Messages => _messages;
+
+	#region commamds
+	#region SendMessageCommand
+	private LambdaCommand? _sendMessageCommand;
+	public ICommand SendMessageCommand => _sendMessageCommand ??= new(OnSendMessageCommandExecute, prop => prop is string { Length: > 0 });
+
+	private void OnSendMessageCommandExecute(object? p)
+	{
+
+	}
+	#endregion
+
+	#region OpenMainWindowCommand
+	private LambdaCommand? _openMainWindowCommand;
+	public ICommand OpenMainWindowCommand => _openMainWindowCommand ??= new(OnOpenMainWindowCommandExecuted, () => true);
+
+	private void OnOpenMainWindowCommandExecuted()
+	{
+
+	}
+	#endregion
+
+	#region ChangeToMainWindowCommand
+	private LambdaCommand? _changeToMainWindowCommand;
+	public ICommand ChangeToMainWindowCommand => _openMainWindowCommand ??= new(OnChangeToMainWindowCommandExecuted, () => true);
+
+	private void OnChangeToMainWindowCommandExecuted()
+	{
+
+	}
+	#endregion
+	#endregion
+
+
+
 }
