82070ea8db349d27f1a6bb34f78ef242ad9ba565
Евгений Нестеров <e.y.nesterov@gmail.com>
2023-01-08 17:23:13 +0300

    Созданы VM главного и второго окна

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace TwoWindowsMVVM.ViewModels;
+
+internal class MainWindowViewModel: ViewModelBase
+{
+	public string Title { get; set; } = "Главное окно";
+}
---
+++ ViewModels/SecondaryWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TwoWindowsMVVM.ViewModels;
+
+internal class SecondaryWindowViewModel : ViewModelBase
+{
+	public string Title { get; set; } = "Второе окно";
+}
